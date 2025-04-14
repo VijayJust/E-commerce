2379c1c3c65e6aabb83613530eb77d59e684034a
VijayJust <vijaysundar92301@gmail.com>
2025-04-14 19:48:47 +0530

    Update E-commerce.java

--- E-commerce.java
+++ E-commerce.java
@@ -5,6 +5,7 @@ public class E commerce
       system.out.println("Booking complete")
         system.out.println("order reached")
         system.out.println("Booking pending")
+        system.out.prinln("Vijay")
         }
   }
     
